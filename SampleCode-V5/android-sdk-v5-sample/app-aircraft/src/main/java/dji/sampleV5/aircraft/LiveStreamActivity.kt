d10f9d8f6f2a7ede234e95eb98a5f57689993ebb
reis756 <reis756004@gmail.com>
2023-11-21 16:17:54 -0300

    vpn implemented

---
+++ SampleCode-V5/android-sdk-v5-sample/app-aircraft/src/main/java/dji/sampleV5/aircraft/LiveStreamActivity.kt
@@ -0,0 +1,17 @@
+package dji.sampleV5.aircraft
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.commit
+import dji.sampleV5.modulecommon.pages.DefaultLayoutTestFragment
+
+class LiveStreamActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_live_stream)
+
+        supportFragmentManager.commit {
+            replace(R.id.nav_host_fragment_content_main, DefaultLayoutTestFragment())
+        }
+    }
+}
